cd4e47dee3e65cf7062ce72067e09e41cab0596f
Ricky Clegg <ricky.clegg@transferwise.com>
2024-07-02 20:20:49 +0100

    feat: solution

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Running application...");
 
-        StockMarket ftse = new StockMarket("London Stock Exchange");
+        Client techTrader = new Client("Tech Trader");
+        Client financeTrader = new Client("Finance Trader");
+
+        StockMarket ftse = new StockMarket("London Stock Exchange", Arrays.asList(techTrader, financeTrader));
+        StockMarket nasdaq = new StockMarket("Nasdaq", List.of(techTrader));
 
         ftse.startTrading();
+        nasdaq.startTrading();
     }
 }
--- src/StockMarket.java
+++ src/StockMarket.java
@@ -1,11 +1,38 @@
+import java.util.List;
+
 public class StockMarket {
     public String name;
+    private final List<Client> clients;
 
-    public StockMarket(String name) {
+    public StockMarket(String name, List<Client> clients) {
         this.name = name;
+        this.clients = clients;
     }
 
     public void startTrading() {
         System.out.println(name + " is open for trading!");
+
+        // Creating stock
+        Stock wiseStock = new Stock(name + " - Wise PLC");
+        Stock appleStock = new Stock(name + " - Apple Inc");
+
+        // Registering clients
+        for (Client client : clients) {
+            wiseStock.registerObserver(client);
+            appleStock.registerObserver(client);
+        }
+
+        // Simulating price changes
+        wiseStock.setPrice(150.0f);
+        wiseStock.setPrice(155.0f);
+        appleStock.setPrice(444.0f);
+
+        // Unregistering a client
+        wiseStock.removeObserver(clients.getFirst());
+
+        // Simulating more price changes
+        wiseStock.setPrice(160.0f);
+        wiseStock.setPrice(165.0f);
+        appleStock.setPrice(445.0f);
     }
 }
