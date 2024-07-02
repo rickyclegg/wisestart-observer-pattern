a73fdb1eba6c4d8d4890534633047d028da4a2bf
Ricky Clegg <ricky.clegg@transferwise.com>
2024-07-02 19:57:15 +0100

    feat: add a hint

--- src/StockMarket.java
+++ src/StockMarket.java
@@ -7,5 +7,15 @@ public class StockMarket {
 
     public void startTrading() {
         System.out.println(name + " is open for trading!");
+
+        // Creating stock
+        Stock appleStock = new Stock("Apple");
+
+        // Creating clients
+        Client client1 = new Client("Tech Trader");
+
+        // Registering clients to the stock
+        appleStock.registerObserver(client1);
+
     }
 }
